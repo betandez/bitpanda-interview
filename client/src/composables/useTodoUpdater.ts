b6d6b29041b8a8ce8fab5d98d079cdcb45abc82f
Maximiliano Hernández <max.hernandezb@gmail.com>
2021-02-08 11:47:34 +0100

    Corrected interface naming

--- client/src/composables/useTodoUpdater.ts
+++ client/src/composables/useTodoUpdater.ts
@@ -2,12 +2,12 @@ import { Ref, ref } from '@vue/composition-api';
 
 import { putTodo, Todo } from '../services/Todo';
 
-interface useTodoUpdatedObject {
+interface useTodoUpdaterObject {
   todoUpdated: Ref;
   updateTodo: () => Promise<void>;
 }
 
-export default function useTodoUpdater(todo: string, isChecked: Ref): useTodoUpdatedObject {
+export default function useTodoUpdater(todo: string, isChecked: Ref): useTodoUpdaterObject {
   const todoUpdated = ref({});
   const updateTodo = async (): Promise<void> => {
     todoUpdated.value = (await putTodo(todo, !isChecked.value)) as Todo;
